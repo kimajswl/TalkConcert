c2a0a8eaf38301e9e6c66adca9f2e0cdef77e20f
Kimajswl <ajswl4763@naver.com>
2024-03-17 23:04:56 +0900

    docker compose file 실행 성공

---
+++ src/test/java/com/example/talkconcert/TalkConcertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talkconcert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkConcertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
